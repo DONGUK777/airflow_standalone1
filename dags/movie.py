5b8c244ff82f35140e834617241821971e02d9bc
DONGUK777 <kimdongyork@gmail.com>
2024-08-01 15:53:23 +0900

    fix

--- dags/movie.py
+++ dags/movie.py
@@ -43,8 +43,18 @@ with DAG(
     def fun_multi_y(ds_nodash, arg):
         from mov.api.call import save2df
         df = save2df(load_dt=ds_nodash, url_param=arg)
-        print(df.head(5))
-    
+        
+        print(df[['movieCd', 'movieNm']].head(5))
+
+        for k, v in url_param.items():
+            df[k] = v
+
+        #p_cols = list(url_param.keys()).insert(0, 'load_dt')
+        p_cols = ['load_dt'] + list(url_param.keys())
+        df.to_parquet('~/tmp/test_parquet',
+                partition_cols=p_cols
+                # partition_cols=['load_dt', 'movieKey']
+        )
 
 
     def save_data(ds_nodash):
--- dags/movie_summary.py
+++ dags/movie_summary.py
@@ -13,6 +13,7 @@ from airflow.operators.python import PythonVirtualenvOperator
 from pprint import pprint
 from airflow.utils.task_group import TaskGroup
 
+
 with DAG(
     'movie_summary' ,
     # These args will get passed on to each operator
@@ -35,26 +36,81 @@ with DAG(
     tags=['api', 'movie', 'summary'],
 
 ) as dag:
-    
-    apply_type = EmptyOperator(
-        task_id = "apply.type",
-    )
+    REQUIREMENTS = [
+                "git+https://github.com/DONGUK777/mov_agg.git@0.5/agg",
+                ]
+    def gen_empty(*ids):
+        tasks = []
+        for id in ids:
+            task = EmptyOperator(task_id=id)
+            tasks.append(task)
+        return tasks
 
-    merge_df = EmptyOperator(
-            task_id="merge.df",
-    )
+    def gen_vpython(**kw):
+        task = PythonVirtualenvOperator(
+            task_id=kw['id'],
+            python_callable=kw['fun_obj'],
+            system_site_packages=False,
+            requirements=REQUIREMENTS,
+            op_kwargs=kw['op_kw']
+            )
+        return task
     
-    de_dup = EmptyOperator(
-            task_id="de.dup",
-    )
+    def pro_data(**params):
+        from pprint import pprint as pp
+        print("@" * 33)
+        print(params['task_name'])
+        pp(params) # 여기는 task_name
+        print("@" * 33)
 
-    summary_df = EmptyOperator(
-            task_id="summary.df",
-    )
-    
+    def pro_data2(task_name, **params):
+        from pprint import pprint as pp
+        print("@" * 33)
+        print(task_name)
+        pp(params) # 여기는 task_name 없을 것으로 예상
+        if "task_name" in params.keys():
+            print("============== 있음")
+        else:
+            print("============== 없음")
+        print("@" * 33)
 
-    start = EmptyOperator(task_id='start')
-    end = EmptyOperator(task_id='end', trigger_rule="all_done") 
+    def pro_data3(task_name):
+        print("@" * 33)
+        print(task_name)
+        #print(params) 
+        print("@" * 33)
     
-    start >> apply_type >> merge_df
-    merge_df >> de_dup >> summary_df >> end
+    def pro_data4(task_name, ds_nodash, **kwargs):
+        from pprint import pprint as pp
+        print("@" * 33)
+        print(task_name)
+        print(ds_nodash)
+        pp(kwargs) 
+        print("@" * 33)
+    
+    start, end = gen_empty('start', 'end')
+    
+    apply_type = gen_vpython(
+            id = "appply.type",
+            fun_obj = pro_data,
+            op_kw = { "task_name": "apply_type!!!" }
+            )
+    merge_df = gen_vpython(
+            id = "merge.df",
+            fun_obj = pro_data2,
+            op_kw = { "task_name": "merge_df!!!" }
+            )
+    de_dup = gen_vpython(
+            id = "de.dup",
+            fun_obj = pro_data3,
+            op_kw = { "task_name": "du_dup!!!" }
+            )
+    summary_df = gen_vpython(
+            id = "summary.df",
+            fun_obj = pro_data4,
+            op_kw = { "task_name": "summary_df!!!" }
+            )
+
+    start >> merge_df
+    merge_df >> de_dup >> apply_type
+    apply_type >> summary_df >> end
